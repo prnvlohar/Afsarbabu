ed9dbb65451c39abdc4be41ee39f02c581cf77c1
pranavlohar19 <144672776+pranavlohar19@users.noreply.github.com>
2024-01-05 17:03:46 +0530

    Update settings.py

--- Sharma_Academy/settings.py
+++ Sharma_Academy/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ghv#+wl($1jh8f4%x7k9g6ohvs4^iyc9bah(ev7)=*ixlgh1du'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
